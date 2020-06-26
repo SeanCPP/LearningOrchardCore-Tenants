620dd341b99e6c85001aa7f6e2055fe54ac8d0da
Sean Franklin <sean.patrick516@gmail.com>
2020-06-26 13:44:45 -0400

    initial commit

---
+++ WebApi2/Controllers/HomeController.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+
+namespace WebApi2.Controllers
+{
+    [Route("[controller]")]
+    public class HomeController : Controller
+    {
+        [HttpGet]
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
